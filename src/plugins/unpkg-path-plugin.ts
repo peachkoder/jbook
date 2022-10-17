6aa50bbca7ab0e7416ff028ffb48195bc3404622
peachkoder <peachkoder@gmail.org>
2022-10-17 22:09:02 +0100

    fixing a TS error: result->esbuild.OnLoadResult

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -62,14 +62,14 @@ export const unpkgPathPlugin = () => {
         }  
         // Check to see if we have already fetched this file
         // and if it is in the cache
-        const cachedResult = await fileCache.getItem(args.path);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
         if (cachedResult){
           return cachedResult;
         }
         // loading from https://unpkg.com/tiny-test-pkg@1.0.0/index.js
         const {data, request} = await axios.get(args.path);
         // console.log( new URL('./', request.responseURL).pathname);
-        const result =  {
+        const result: esbuild.OnLoadResult =  {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname //  "https://unpkg.com/nested-test-pkg@1.0.0/src/index.js"
