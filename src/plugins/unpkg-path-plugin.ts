296d30a710593bbdb30965765a0009db0bfc4ff5
peachkoder <peachkoder@gmail.org>
2022-10-17 19:32:40 +0100

    resolving nested paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -16,7 +16,7 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../') ) {
           return {
             namespace: 'a',
-            path: new URL(args.path, args.importer + '/').href
+            path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
           }
         }
 
@@ -40,16 +40,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
         }  
         // loading from https://unpkg.com/tiny-test-pkg@1.0.0/index.js
-        const {data} = await axios.get(args.path);
+        const {data, request} = await axios.get(args.path);
+        console.log( new URL('./', request.responseURL).pathname);
         return {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname //  "https://unpkg.com/nested-test-pkg@1.0.0/src/index.js"
         }
       });
     },
