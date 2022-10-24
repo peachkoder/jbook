a61f2d9872eb1bf5275fcafb6b472286b61d6728
peachkoder <peachkoder@gmail.org>
2022-10-24 19:58:28 +0100

    tricking esbuild's css handling

--- src/plugins/fetch-pugin.ts
+++ src/plugins/fetch-pugin.ts
@@ -32,13 +32,28 @@ export const fetchplugin = (inputCode: string) => {
           return cachedResult;
         }
 
-        const loader = args.path.match(/.css$/) ? 'css' : 'jsx';
         // loading from https://unpkg.com/tiny-test-pkg@1.0.0/index.js
         const {data, request} = await axios.get(args.path);
+
+        // format the data string in order to avoid conflits when it is inserted
+        // inside de style.innerText tag
+        const escaped = data
+          .replace(/\n/, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+        const contents = fileType === 'css' 
+            ?
+          ` const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          ` : data;
+
         // console.log( new URL('./', request.responseURL).pathname);
         const result: esbuild.OnLoadResult =  {
-          loader,
-          contents: data,
+          loader: 'jsx',
+          contents,
           resolveDir: new URL('./', request.responseURL).pathname //  "https://unpkg.com/nested-test-pkg@1.0.0/src/index.js"
         }
          // store response in cache
